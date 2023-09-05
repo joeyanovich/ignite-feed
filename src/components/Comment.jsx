bab4820cf7b86b661de7fc86b76063e96007fb71
joeyanovich <joedison.dias@gmail.com>
2023-09-04 23:36:29 -0300

    feat 🧑‍💻: created comment structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/joeyanovich.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Joey Dias</strong>
+                            <time title='31 de agosto às 22h44' dateTime='2023-08-31 22:43:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
