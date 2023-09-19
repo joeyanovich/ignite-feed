39c34782245f955ccc9f93bcbc17cbb63ff28466
joeyanovich <joedison.dias@gmail.com>
2023-09-18 22:29:24 -0300

    feat 🧑‍💻: declarative programming in comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,13 +7,14 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 import { useState } from 'react'
 
-const comments = [
-	1,
-	2,
-]
+// const comments = [
+// 	'Post muito bacana, hein!?'
+// ]
 
 export function Post({ author, publishedAt, content }) {
-	const [comments, setComments] = useState([1, 2])
+	const [comments, setComments] = useState(['Post muito bacana, hein!?'])
+
+	const [newCommentText, setNewCommentText] = useState('')
 
 	const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH'h'mm", {
 		locale: ptBR,
@@ -27,7 +28,16 @@ export function Post({ author, publishedAt, content }) {
 	function handleCreateNewComment() {
 		event.preventDefault()
 		
-		setComments([...comments, comments.length + 1])
+		// const newContentText = event.target.comment.value
+
+		setComments([...comments, newCommentText])
+		setNewCommentText('')
+
+		// event.target.comment.value = ''
+	}
+
+	function handleNewCommentChange() {
+		setNewCommentText(event.target.value)
 	}
 
     return (
@@ -59,8 +69,11 @@ export function Post({ author, publishedAt, content }) {
 
 			<form onSubmit={handleCreateNewComment} className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
-				<textarea 
+				<textarea
+					name='comment'
 					placeholder='Deixe um comentário'
+					value={newCommentText}
+					onChange={handleNewCommentChange}
 				/>
 				<footer>
 					<button type='submit'>Publicar</button>
@@ -69,7 +82,7 @@ export function Post({ author, publishedAt, content }) {
 
 			<div className={styles.commentList}>
 				{comments.map(comment => {
-					return <Comment />
+					return <Comment content={comment}/>
 				})}
 			</div>
         </article>
