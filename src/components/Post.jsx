cd4fb2fbd2065a78f24ee13685ba058f3fe7f331
joeyanovich <joedison.dias@gmail.com>
2023-09-18 23:15:01 -0300

    feat 🥅: inserting keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,9 +7,6 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 import { useState } from 'react'
 
-// const comments = [
-// 	'Post muito bacana, hein!?'
-// ]
 
 export function Post({ author, publishedAt, content }) {
 	const [comments, setComments] = useState(['Post muito bacana, hein!?'])
@@ -27,13 +24,10 @@ export function Post({ author, publishedAt, content }) {
 
 	function handleCreateNewComment() {
 		event.preventDefault()
-		
-		// const newContentText = event.target.comment.value
 
 		setComments([...comments, newCommentText])
 		setNewCommentText('')
 
-		// event.target.comment.value = ''
 	}
 
 	function handleNewCommentChange() {
@@ -60,9 +54,9 @@ export function Post({ author, publishedAt, content }) {
 			<div className={styles.content}>
 				{content.map(line => {
 					if (line.type === 'paragraph') {
-						return <p>{line.content}</p>;
+						return <p key={line.content}>{line.content}</p>;
 					} else if (line.type === 'link') {
-						return <p><a href="#">{line.content}</a></p>
+						return <p key={line.content}><a href="#">{line.content}</a></p>
 					}
 				})}
 			</div>
@@ -82,7 +76,7 @@ export function Post({ author, publishedAt, content }) {
 
 			<div className={styles.commentList}>
 				{comments.map(comment => {
-					return <Comment content={comment}/>
+					return <Comment key={comment} content={comment}/>
 				})}
 			</div>
         </article>
