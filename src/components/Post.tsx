5e29a49992620949d51796d645a7d19f16c97e65
joeyanovich <joedison.dias@gmail.com>
2023-09-21 22:33:26 -0300

    feat 🧑‍💻: finished project

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -13,10 +13,14 @@ interface Author {
 	avatarUrl: string;
 }
 
-interface PostProps {
+export interface PostType {
+	id: number;
 	author: Author;
 	publishedAt: Date;
 	content: Content[];
+}
+interface PostProps {
+	post: PostType;
 }
 
 interface Content {
@@ -25,16 +29,16 @@ interface Content {
 }
 
 
-export function Post({ author, publishedAt, content }: PostProps) {
+export function Post({ post }: PostProps) {
 	const [comments, setComments] = useState(['Post muito bacana, hein!?'])
 
 	const [newCommentText, setNewCommentText] = useState('')
 
-	const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH'h'mm", {
+	const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH'h'mm", {
 		locale: ptBR,
 	})
 
-	const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+	const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
 		locale: ptBR,
 		addSuffix: true,
 	})
@@ -67,21 +71,21 @@ export function Post({ author, publishedAt, content }: PostProps) {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={author.avatarUrl}/>
+                    <Avatar src={post.author.avatarUrl}/>
                 
                 	<div className={styles.authorInfo}>
-						<strong>{author.name}</strong>
-						<span>{author.role}</span>
+						<strong>{post.author.name}</strong>
+						<span>{post.author.role}</span>
                 	</div>
 				</div>
 
-				<time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+				<time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>
 					{publishedDateRelativeToNow}
 				</time>
             </header>
 
 			<div className={styles.content}>
-				{content.map(line => {
+				{post.content.map(line => {
 					if (line.type === 'paragraph') {
 						return <p key={line.content}>{line.content}</p>;
 					} else if (line.type === 'link') {
